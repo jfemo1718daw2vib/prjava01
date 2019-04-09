1a794c8edef207f511e0f262287e4771f749458c
alumne <alumne@ac002-12>
2019-04-09 09:39:44 +0200

    Versió 0.1 del projecte prjava00

---
+++ prjava00.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumne
+ */
+public class prjava00 {
+    public static void main(String[] args) {
+        System.out.println("Hola món"); 
+        System.out.println("Versió 0.1 del projecte prjava00");
+    }
+}
